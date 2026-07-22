c299ac52224f498f581e3660e6450172b90cda75
Keshav Maiya <keshavrajmaiya@gmail.com>
2026-07-22 15:44:51 +0530

    Fix RAW data path in paths.py to include the correct file

--- src/paths.py
+++ src/paths.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 PROJECT_ROOT = Path('/content/drive/MyDrive/Nepali-Decoder')
 
-RAW =  PROJECT_ROOT / "RAW_DATA"
+RAW =  PROJECT_ROOT / "Raw_Data" / "nepali-books.json"
 
 CHUNKS = PROJECT_ROOT / "Chunked_Data_Raw"
